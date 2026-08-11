19afe8f2e3b51c8c92b21498a040707ae30e40f5
ankita642 <jadhavankita116@gmail.com>
2026-08-11 10:26:13 +0530

    initial changes

---
+++ Array/reverse.py
@@ -0,0 +1,38 @@
+from array import *
+
+val = array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+for i in range (0, len(val)):
+    print(val[i], end= " ")
+
+print ('\n')
+
+for x in val:
+    print(x, end= ' , ')
+
+from array import *
+
+val = array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+for i in range (0, len(val)):
+    print(val[i], end= " ")
+
+print ('\n')
+
+for x in val:
+    print(x, end= ' , ')
+
+print ('\n')
+
+print (val.typecode)
+
+print ('\n')
+
+val.reverse()
+
+for i in range (0, len(val)):
+    print(val[i], end= " ")
+
+
+
+
